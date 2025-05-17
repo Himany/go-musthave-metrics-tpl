a540a2d394ab22502f2f535ff419c1db8b2a5cfb
Himany <Himaaan@yandex.ru>
2025-05-17 22:59:54 +0300

    add logger, fix agent error handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 
 	"github.com/Himany/go-musthave-metrics-tpl/handlers"
+	"github.com/Himany/go-musthave-metrics-tpl/internal/logger"
 	"github.com/Himany/go-musthave-metrics-tpl/server"
 	"github.com/Himany/go-musthave-metrics-tpl/storage"
 )
 
 func main() {
-	parseFlags()
+	if err := parseFlags(); err != nil {
+		panic("failed to initialize flags: " + err.Error())
+	}
+
+	if err := logger.Initialize(logLevel); err != nil {
+		panic("failed to initialize logger: " + err.Error())
+	}
 
 	memStorage := storage.NewMemStorage()
 	handler := &handlers.Handler{Repo: memStorage}
 
 	if err := server.Run(handler, runAddr); err != nil {
-		log.Fatal(err)
+		logger.Log.Fatal("main", zap.Error(err))
 	}
 }
